50c226f8eefdc779f465b79bf79b50026155adf9
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-06-28 23:46:50 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -51,7 +51,7 @@ export default function Home() {
           duration: 0.5,
         }}
         className="fixed bottom-4 right-4 text-sm text-white/30 pointer-events-none z-50 font-light">
-        Meri cute si biwi 💕
+        Meri cute si Aru 💕
       </motion.div>
     </div>
   );
